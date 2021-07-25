c2555907c3c8a95124ed53b2b8c0ce16e1f82fd6
Amandeep Singh <amandeep.bajwa@care.com>
2021-07-26 00:30:44 +0530

    feat: added events

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,56 @@
 import Head from 'next/head'
-// import Image from 'next/image'
-import heroImg from '../public/img/hero.jpg'
+import Image from 'next/image'
+import logo from '../public/img/logo.png'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <Head>
         <title>AWS User Group Punjab</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className={styles.main}>
-        {/* <Image src={heroImg} alt="AWS User Group Punjab" /> */}
+      <section className={styles.hero}>
+        <Image src={logo} alt="AWS User Group Punjab" width={150} height={150} />
         <h1 className={styles.title}>
           Welcome to <span>AWS User Group Punjab</span>
         </h1>
 
         <p className={styles.description}>
-          A place where all AWS enthusiasts meet, learn and share!
+          A place where AWS enthusiasts meet, learn and share!
         </p>
+      </section>
 
-        <div>
-          Our first meet up will take place on 31st July, 2021 - Saturday at 11:00 am IST.
-        </div>
+      <section className={styles.content}>
+        <h2>Upcoming Meetups:</h2>
 
-        <div>More details will be added soon.</div>
-      </main>
-    </div>
+        <ol>
+          <li>
+            <h3>Agenda: Static Web Apps with AWS Amplify</h3>
+            <p>
+              Our first meet up will take place on 31st July, 2021 - Saturday at 11:00 am IST.
+            </p>
+
+            <p>
+              Register <a href="https://docs.google.com/forms/d/e/1FAIpQLScD7Kb3gTqV6M-983cApuCtI3zvKJodhiql_Z_Lq4TfKvNU9w/viewform" target="_blank">here</a>
+            </p>
+          </li>
+
+          <li>
+            <h3>Agenda: Creating REST APIs with AWS Amplify</h3>
+            <p>
+              Our second meet up will take place on 14th Aug, 2021 - Saturday at 11:00 am IST.
+            </p>
+
+            <p>
+              Register <a href="https://docs.google.com/forms/d/1WIlUXtEF0nJbQeEXAOeKocR2FoRYYuFh-YS2bW-5BB4" target="_blank">here</a>
+            </p>
+          </li>
+        </ol>
+
+        <p><a href="https://join.slack.com/t/aws-user-group-punjab/shared_invite/zt-sb9mehdf-oBnMehf5J0GhVhlr2RB3hg">Join our Slack Channel</a></p>
+      </section>
+    </main>
   )
 }
